208bd6e0bb9f7ebe4152eecd985d3d1910352b75
kiyoto_777@yahoo.co.jp <kiyoto_777@yahoo.co.jp@d1fdeaaf-f0d8-a22b-1e7e-0722d37d6d22>
2013-11-14 01:37:30 +0000

    t

---
+++ test20131114_dw.image.js
@@ -0,0 +1,17 @@
+var image_pload = {"Pimages":[
+'image/imgs/file01on.jpg',
+'image/imgs/file02on.jpg',
+'image/imgs/file03on.jpg',
+'image/imgs/file04on.jpg',
+'image/imgs/file05on.jpg',
+'image/imgs/file06on.jpg',
+'image/imgs/file07on.jpg',
+'image/imgs/file08on.jpg',
+'image/imgs/file09on.jpg',
+'image/imgs/file10on.jpg',
+'image/imgs/file11on.jpg',
+'image/imgs/file12on.jpg',
+'image/imgs/file13on.jpg',
+'image/imgs/file14on.jpg',
+'image/imgs/file15on.jpg'
+]};
